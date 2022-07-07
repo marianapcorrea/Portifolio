2805243ebc862f0ce9c7273ff4f8495543034bc8
Mariana Correa <mpcs270391@gmail.com>
2022-07-07 10:08:04 -0300

    Insere espaço p para colocar link do repositório

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,6 +5,7 @@ const Footer = () => {
     return (
         <div className={styles.footer}>
             <p>Copyrights Créditos</p>
+            <p>Veja o código desse repositório no GitHub</p>
         </div>
     );
 };
