26e393d04c581f79c479d3d7501d27c8e6249a42
Mariana Correa <mpcs270391@gmail.com>
2022-07-11 11:01:49 -0300

    Estiliza componente de midias sociais

--- src/Components/Main/Contact/Contact.js
+++ src/Components/Main/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./Contact.module.css";
 import photo from "../../../Assets/photo.png";
+import ContactSocialMediaList from "./ContactSocialMediaList";
 
 const Contact = () => {
     return (
@@ -10,14 +11,15 @@ const Contact = () => {
             </div>
             <div className={styles.email}>
                 <p>Fale comigo!</p>
-                <h3>mpcs270391@gmail.com</h3>
+                <h3>
+                    <a href="mailto: mpcs270391@gmail.com">
+                        mpcs270391@gmail.com
+                    </a>
+                </h3>
             </div>
             <div className={styles.socialMedia}>
                 <h3>Redes Sociais</h3>
-                <ul>
-                    <li>LinkedIn</li>
-                    <li>GitHub</li>
-                </ul>
+                <ContactSocialMediaList />
             </div>
         </section>
     );
