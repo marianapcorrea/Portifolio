60dba3f84cb9a8b0c648891052062e5596e2ad8f
Mariana Correa <mpcs270391@gmail.com>
2022-07-15 14:07:19 -0300

    Modifica link para home

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "./Header.module.css";
 import HeaderNav from "./HeaderNav";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 const Header = () => {
     return (
         <header className={styles.header}>
-            <a className={`${styles.link} toHome`} href="/" aria-label="Home">
+            <AnchorLink
+                id="linkHome"
+                href="#home"
+                className={`${styles.link} toHome`}
+                aria-label="Home"
+            >
                 marianapcorrea
-            </a>
+            </AnchorLink>
             <HeaderNav className={styles.nav} />
         </header>
     );
