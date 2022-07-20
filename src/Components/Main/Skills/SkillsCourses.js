52de0eeda15e8d4e2943a71bd025fdb9674f09fa
Mariana Correa <mpcs270391@gmail.com>
2022-07-20 19:21:31 -0300

    Cria e estiliza estrutura para card de cursos

--- src/Components/Main/Skills/SkillsCourses.js
+++ src/Components/Main/Skills/SkillsCourses.js
@@ -1,15 +1,18 @@
 import React from "react";
+import styles from "./SkillsCourses.module.css";
 
 const SkillsCourses = () => {
     return (
-        <section>
+        <section className={styles.courses}>
             <h1>Cursos e Formações</h1>
-            <div>
-                <p>Nome do Cursos</p>
-                <p>Escola</p>
-                <div>
-                    <p>Carga Horária</p>
-                    <p>Ano</p>
+            <div className={styles.card}>
+                <h3>Nome do Cursos</h3>
+                <div className={styles.subcontainer}>
+                    <div className={styles.info}>
+                        <p>Escola</p>
+                        <p>Carga Horária</p>
+                    </div>
+                    <p className={styles.year}>Ano</p>
                 </div>
             </div>
         </section>
