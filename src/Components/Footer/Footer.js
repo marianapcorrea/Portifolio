afdf02425d94e083bbe4ca3ed0f29433b59d4323
Mariana Correa <mpcs270391@gmail.com>
2022-07-22 12:15:25 -0300

    Altera estrutura e estiliza footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -4,8 +4,20 @@ import styles from "./Footer.module.css";
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <p>Copyrights Créditos</p>
-            <p>Veja o código desse repositório no GitHub</p>
+            <p className={styles.credits}>
+                Desenvolvido por &nbsp;
+                <span className={styles.myName}> Mariana P. Corrêa</span>.
+                &nbsp;{" "}
+                <span className={styles.copyright}>
+                    Copyrights Copyrights Copyrights
+                </span>
+            </p>
+
+            <div className={styles.repo}>
+                <p>
+                    Veja o código desse site no <a href="/"> GitHub</a>
+                </p>
+            </div>
         </div>
     );
 };
