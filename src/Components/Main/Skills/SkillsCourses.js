4ab6a5e6a5763320d57b88cb2456527cc87a4c36
Mariana Correa <mpcs270391@gmail.com>
2022-07-22 10:58:41 -0300

    Cria função de map e objeto com info de cursos

--- src/Components/Main/Skills/SkillsCourses.js
+++ src/Components/Main/Skills/SkillsCourses.js
@@ -2,19 +2,50 @@ import React from "react";
 import styles from "./SkillsCourses.module.css";
 
 const SkillsCourses = () => {
+    const coursesList = [
+        {
+            course: "Curso",
+            institute: "Escola",
+            workload: "XXX",
+            conclusion: "2022",
+        },
+        {
+            course: "Curso",
+            institute: "Escola",
+            workload: "XXX",
+            conclusion: "2022",
+        },
+        {
+            course: "Curso",
+            institute: "Escola",
+            workload: "XXX",
+            conclusion: "2022",
+        },
+        {
+            course: "Curso",
+            institute: "Escola",
+            workload: "XXX",
+            conclusion: "2022",
+        },
+    ];
+
+    const mapCourses = coursesList.map((c, i) => (
+        <div key={c.i} className={styles.card}>
+            <h3>{c.course}</h3>
+            <div className={styles.subcontainer}>
+                <div className={styles.info}>
+                    <p>{c.institute}</p>
+                    <p>{c.workload} horas</p>
+                </div>
+                <p className={styles.conclusion}>{c.conclusion}</p>
+            </div>
+        </div>
+    ));
+
     return (
         <section className={styles.courses}>
             <h1>Cursos e Formações</h1>
-            <div className={styles.card}>
-                <h3>Nome do Cursos</h3>
-                <div className={styles.subcontainer}>
-                    <div className={styles.info}>
-                        <p>Escola</p>
-                        <p>Carga Horária</p>
-                    </div>
-                    <p className={styles.year}>Ano</p>
-                </div>
-            </div>
+            <div className={styles.cardContainer}>{mapCourses}</div>
         </section>
     );
 };
