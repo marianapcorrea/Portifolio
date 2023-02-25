1b64ed96d26f72a3fcd6b60fedccf9d32ac775ff
Mariana Correa <mpcs270391@gmail.com>
2023-02-25 16:24:07 -0300

    Add final text of content

--- src/Components/Main/About/About.js
+++ src/Components/Main/About/About.js
@@ -6,24 +6,44 @@ const About = () => {
     return (
         <section id="about" className={`pageContainer ${styles.about}`}>
             <section className={styles.aboutText}>
-                <h1>Sobre Mim</h1>
+                <h1>Um pouco sobre mim...</h1>
                 <p>
-                    Quisque ac dolor eros. Cras pretium ac est quis condimentum.
-                    Cras non ante sed justo volutpat viverra vel eget ex. Duis
-                    sed lacus elementum, faucibus metus quis, rhoncus tortor.
-                    Mauris vitae nunc mollis, tincidunt felis eget, interdum
-                    libero. Duis accumsan, lacus vitae fermentum bibendum, enim
-                    felis tincidunt ante, eu posuere augue arcu at sem.{" "}
+                    Olá, eu sou a Mariana, desenvolvedora front-end, e é um prazer ter você em meu
+                    portifólio.
                 </p>
                 <p>
-                    Maecenas nisl est, euismod et erat non, volutpat faucibus
-                    risus. Sed tristique, purus in vehicula convallis, mi enim
-                    rhoncus justo, a venenatis nulla arcu nec dui. Mauris id
-                    odio ornare, tincidunt massa in, bibendum velit. Aliquam
-                    molestie aliquet ligula, eget sodales ex elementum vel.
-                    Integer in nisl ac dui scelerisque faucibus. Nulla feugiat
-                    nisi vel augue volutpat pretium.
+                    Atualmente, vivo em Belo Horizonte, Minas Gerais com minha parceira e nossos
+                    seis gatinhos.
                 </p>
+                <p>
+                    Foi a minha parceira quem me apresentou o incrível mundo da tecnologia, onde me
+                    apaixonei pela arte de fazer códigos e resolvi mudar da minha carreira em
+                    Conservação e Restauração de Bens Culturais para me tornar desenvolvedora web.
+                </p>
+                <p>Hoje, atuo como freelancer, desenvolvendo a interface gráfica de páginas web.</p>
+                <h2>O que posso fazer por você:</h2>
+                <ul>
+                    <li>
+                        Execução de projetos, usando{" "}
+                        <strong>HTML semântico, CSS, JavaScript, frameworks e bibliotecas</strong>;
+                    </li>
+                    <li>
+                        Implementação de <strong>designs responsivos</strong> para sites e
+                        interfaces;
+                    </li>
+                    <li>
+                        Criação de ferramentas e códigos para <strong>consumo de API's</strong>;
+                    </li>
+                    <li>
+                        <strong>Refatoração de páginas HTML</strong> para implementar semântica e
+                        melhorar o suporte ao <strong>SEO</strong> (Search Engine Optimization) e{" "}
+                        <strong>acessibilidade</strong> do site;
+                    </li>
+                    <li>Manutenção de sites;</li>
+                    <li>
+                        Priorização e suporte à <strong>experiência do usuário</strong>.
+                    </li>
+                </ul>
             </section>
             <section className={styles.imgContainer}>
                 <img src={photo} alt="Foto frontal de Mariana Corrêa" />
